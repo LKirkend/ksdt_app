8402ffe421c234d5c729a323f619df114112ddad
tritonro <106782621+tritonro@users.noreply.github.com>
2023-12-28 19:00:30 -0800

    basic caching class ?

--- server_caching/cache.ts
+++ server_caching/cache.ts
@@ -1,13 +1,15 @@
-/* This file will contain widgets taken from the API */ 
+/*This file implements fetching from an endpoint and client-side caching */
 
 /**
- * 
+ * This function retrieves JSON data from an endpoint URL 
  * @param url 
- * @returns JSON file fetched 
+ * @param max_age - the maximum age in seconds of the data we want from the endpoint 
+ * @returns Response object with JSON as body (readable stream), and Content-Type header set to application/json 
  */
-export async function serverFetch (url:string) {
+export async function serverFetch (url:string, max_age: number) {
     let head: Headers  = new Headers(); 
     head.append( 'Content-Type', 'application/json')
+    head.append("Cache-Control", "max-age:" + {max_age})
 
     //Check Response.status and Response.ok
     var  res = await fetch(url, {headers: head}).then(
@@ -16,7 +18,6 @@ export async function serverFetch (url:string) {
                 throw new Error ("Response is not ok")
             }
             else {
-                response.json().then((json)=>{console.log(json)}) //this line breaks the code bc stream can only be read 1x
                 var file : Promise<Response> = response.json()
                 return file; 
             }
@@ -24,6 +25,51 @@ export async function serverFetch (url:string) {
         .catch
             ((error) => console.log(error))
         }
+
+/**
+ * This is our caching class
+ * adapted from StackOverflow: https://codereview.stackexchange.com/questions/245714/typescript-simple-localstorage-cache
+ *  
+ */
+interface CacheItem<T> {
+    data: T;
+    expiration: number;
+  }
+  
+  class SimpleLocalStorageCache<T> {
+    durationInMilliseconds: number;
+    constructor(private key: string, private durationInSeconds: number) {
+    this.durationInMilliseconds = this.durationInSeconds * 1000;
+    }
+  
+    get(): CacheItem<T> | null {
+      const cache = localStorage.getItem(this.key);
+      
+      if (!cache) {
+        return null;
+      }
+  
+      const parsedCache = JSON.parse(cache);
+  
+      if (Date.now() >= parsedCache.expiration) {
+        return null;
+      }
+  
+      return parsedCache;
+    }
+  
+    update(data: T extends string | number | boolean): void {
+      localStorage.setItem(
+        this.key,
+        JSON.stringify({
+          data,
+          expiration: Date.now() + this.durationInMilliseconds,
+        })
+      );
+    }
+  }
+  
+  export default SimpleLocalStorageCache;
 
 /*
 class getData {
